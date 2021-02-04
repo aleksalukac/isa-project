0c91683ba78c6d85f5e580b8385a64317952005d
Filip <filipzdelar@gmail.com>
2021-02-04 18:07:35 +0100

    ISA deleted

---
+++ Pharmacy/Pharmacy/Models/Entities/Complaint.cs
@@ -0,0 +1,21 @@
+﻿using Pharmacy.Models.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Pharmacy.Models.Entities
+{
+    
+    [Table("tbComplaints")]
+    public class Complaint
+    {
+        public long Id { get; set; }
+        public AppUser User { get; set; }
+        public AppUser Employee { get; set; }
+        public Pharmacy Pharmacy { get; set; }  
+        public string ReportText { get; set; }
+
+    }
+}
