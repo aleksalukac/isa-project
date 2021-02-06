e50b420736d7bcb77f6e0cd66a3563fe40adf37d
Filip <filipzdelar@gmail.com>
2021-02-06 23:07:25 +0100

    Plot Chart

---
+++ Pharmacy/Pharmacy/Controllers/ColumnChartController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Controllers
+{
+    public class ColumnChartController : Controller
+    {
+        // GET: /<controller>/  
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public class PopulationModel
+        {
+            public string CityName { get; set; }
+            public int PopulationYear2020 { get; set; }
+            public int PopulationYear2010 { get; set; }
+            public int PopulationYear2000 { get; set; }
+            public int PopulationYear1990 { get; set; }
+
+        }
+
+        public static List<PopulationModel> GetCityPopulationList()
+        {
+            var list = new List<PopulationModel>();
+            list.Add(new PopulationModel { CityName = "PURI", PopulationYear2020 = 28000, PopulationYear2010 = 45000, PopulationYear2000 = 22000, PopulationYear1990 = 50000 });
+            list.Add(new PopulationModel { CityName = "Bhubaneswar", PopulationYear2020 = 30000, PopulationYear2010 = 49000, PopulationYear2000 = 24000, PopulationYear1990 = 39000 });
+            list.Add(new PopulationModel { CityName = "Cuttack", PopulationYear2020 = 35000, PopulationYear2010 = 56000, PopulationYear2000 = 26000, PopulationYear1990 = 41000 });
+            list.Add(new PopulationModel { CityName = "Berhampur", PopulationYear2020 = 37000, PopulationYear2010 = 44000, PopulationYear2000 = 28000, PopulationYear1990 = 48000 });
+            list.Add(new PopulationModel { CityName = "Odisha", PopulationYear2020 = 40000, PopulationYear2010 = 38000, PopulationYear2000 = 30000, PopulationYear1990 = 54000 });
+
+            return list;
+
+        }
+
+        [HttpGet]
+        public JsonResult PopulationChart()
+        {
+            var populationList = GetCityPopulationList();
+            return Json(populationList);
+        }
+    }
+}
--- Pharmacy/Pharmacy/Controllers/HomeController.cs
+++ Pharmacy/Pharmacy/Controllers/HomeController.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Pharmacy.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Pharmacy.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
 
         public bool ShowPharmacyToolBar { get; set; }
@@ -23,8 +26,12 @@ namespace Pharmacy.Controllers
 
         public IActionResult Index()
         {
+
+
             return View();
         }
+
+
 
         [Authorize(Roles = "User")]
         public IActionResult Privacy()
