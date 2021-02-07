8dde173573003dd80c820e6d5f082ad69557c246
Filip <filipzdelar@gmail.com>
2021-02-07 12:01:35 +0100

    Drug Report

--- Pharmacy/Pharmacy/Controllers/ColumnChartController.cs
+++ Pharmacy/Pharmacy/Controllers/ColumnChartController.cs
@@ -1,46 +1,125 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pharmacy.Data;
+using Pharmacy.Models.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 namespace Pharmacy.Controllers
 {
     public class ColumnChartController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ColumnChartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: /<controller>/  
         public IActionResult Index()
         {
             return View();
         }
 
+        public class PopulationModelYear
+        {
+            public string Name {get; set;}
+            public double Value { get; set; }/*
+            public double thisYear { get; set; }
+            public double lastYear { get; set; }
+            public double twoYearsAgo { get; set; }
+            public double threeYearsAgo { get; set; }
+            public double fourYearsAgo { get; set; }*/
+        }
+
+        
+
         public class PopulationModel
         {
             public string CityName { get; set; }
             public int PopulationYear2020 { get; set; }
-            public int PopulationYear2010 { get; set; }
-            public int PopulationYear2000 { get; set; }
-            public int PopulationYear1990 { get; set; }
-
         }
 
-        public static List<PopulationModel> GetCityPopulationList()
+        public async Task<List<PopulationModel>> GetCityPopulationListYearAsync()
+        {
+            /*
+            var list = new List<PopulationModelYear>();
+            PopulationModelYear model = new PopulationModelYear();
+
+            model.Name = "This Year";
+            model.Value = (await _context.tbOrders.Where(x => x.TransactionComplete == true && x.TimeOfTransaction.Year == DateTime.Now.Year).ToListAsync()).Sum(item => item.Cost);
+            list.Add(model);
+
+            PopulationModelYear model2 = new PopulationModelYear();
+
+            model2.Name = "Last Year";
+            model2.Value = (await _context.tbOrders.Where(x => x.TransactionComplete == true && x.TimeOfTransaction.Year == DateTime.Now.Year - 1).ToListAsync()).Sum(item => item.Cost);
+            list.Add(model2);*/
+
+
+            var list = new List<PopulationModel>();
+            list.Add(new PopulationModel { CityName = "This Year", PopulationYear2020 = (int)(await _context.tbOrders.Where(x => x.TransactionComplete == true && x.TimeOfTransaction.Year == DateTime.Now.Year).ToListAsync()).Sum(item => item.Cost) });
+            list.Add(new PopulationModel { CityName = "Last Year", PopulationYear2020 = (int)(await _context.tbOrders.Where(x => x.TransactionComplete == true && x.TimeOfTransaction.Year == DateTime.Now.Year - 1).ToListAsync()).Sum(item => item.Cost) });
+            list.Add(new PopulationModel { CityName = "2 Years Ago", PopulationYear2020 = (int)(await _context.tbOrders.Where(x => x.TransactionComplete == true && x.TimeOfTransaction.Year == DateTime.Now.Year - 2).ToListAsync()).Sum(item => item.Cost) });
+            list.Add(new PopulationModel { CityName = "3 Years Ago", PopulationYear2020 = (int)(await _context.tbOrders.Where(x => x.TransactionComplete == true && x.TimeOfTransaction.Year == DateTime.Now.Year - 3).ToListAsync()).Sum(item => item.Cost) });
+
+            
+            return list;
+        }
+
+        public async Task<List<PopulationModel>> GetCityPopulationListQuarterAsync()
         {
             var list = new List<PopulationModel>();
-            list.Add(new PopulationModel { CityName = "PURI", PopulationYear2020 = 28000, PopulationYear2010 = 45000, PopulationYear2000 = 22000, PopulationYear1990 = 50000 });
-            list.Add(new PopulationModel { CityName = "Bhubaneswar", PopulationYear2020 = 30000, PopulationYear2010 = 49000, PopulationYear2000 = 24000, PopulationYear1990 = 39000 });
-            list.Add(new PopulationModel { CityName = "Cuttack", PopulationYear2020 = 35000, PopulationYear2010 = 56000, PopulationYear2000 = 26000, PopulationYear1990 = 41000 });
-            list.Add(new PopulationModel { CityName = "Berhampur", PopulationYear2020 = 37000, PopulationYear2010 = 44000, PopulationYear2000 = 28000, PopulationYear1990 = 48000 });
-            list.Add(new PopulationModel { CityName = "Odisha", PopulationYear2020 = 40000, PopulationYear2010 = 38000, PopulationYear2000 = 30000, PopulationYear1990 = 54000 });
+            list.Add(new PopulationModel { CityName = "First Quarter", PopulationYear2020 = (int)(await _context.tbOrders.Where(x => x.TransactionComplete == true && x.TimeOfTransaction <= DateTime.Now && x.TimeOfTransaction >= DateTime.Now.AddMonths(-3)).ToListAsync()).Sum(item => item.Cost) });
+            list.Add(new PopulationModel { CityName = "Second Quarter", PopulationYear2020 = (int)(await _context.tbOrders.Where(x => x.TransactionComplete == true && x.TimeOfTransaction <= DateTime.Now.AddMonths(-3) && x.TimeOfTransaction >= DateTime.Now.AddMonths(-6)).ToListAsync()).Sum(item => item.Cost) });
+            list.Add(new PopulationModel { CityName = "Third Quarter", PopulationYear2020 = (int)(await _context.tbOrders.Where(x => x.TransactionComplete == true && x.TimeOfTransaction <= DateTime.Now.AddMonths(-6) && x.TimeOfTransaction >= DateTime.Now.AddMonths(-9)).ToListAsync()).Sum(item => item.Cost) });
+            list.Add(new PopulationModel { CityName = "Forth Quarter", PopulationYear2020 = (int)(await _context.tbOrders.Where(x => x.TransactionComplete == true && x.TimeOfTransaction <= DateTime.Now.AddMonths(-9) && x.TimeOfTransaction >= DateTime.Now.AddMonths(-12)).ToListAsync()).Sum(item => item.Cost) });
+
 
             return list;
+        }
 
+        public async Task<List<PopulationModel>> GetCityPopulationListMonthAsync()
+        {
+            var list = new List<PopulationModel>();
+            //var months = (await _context.tbOrders.ToListAsync());
+
+
+            for (int i = 0; i < 12; i++)
+            {
+                list.Add(new PopulationModel { CityName = new DateTime(1, i+1, 1).ToString("MMMM", CultureInfo.CreateSpecificCulture("en-GB")), PopulationYear2020 = (int) (await _context.tbOrders.Where(x => x.TimeOfTransaction.Month == i+1 && (x.TimeOfTransaction.Year == DateTime.Now.Year || x.TimeOfTransaction.Year == DateTime.Now.Year - 1)).ToListAsync()).Sum(x => x.Cost) });
+
+            }
+            /*for (int i = 0; i < trendData.Count; i++)
+            {
+                list.Add(new PopulationModel { CityName = new DateTime(1, i, 1).Month.ToString(), PopulationYear2020 = (int)trendData[i].Sum(x => x.Cost) });
+            }*/
+
+            return list;
         }
 
         [HttpGet]
-        public JsonResult PopulationChart()
+        public JsonResult PopulationChartYear()
         {
-            var populationList = GetCityPopulationList();
+            var populationList = GetCityPopulationListYearAsync().Result;
+            return Json(populationList);
+        }
+        
+        [HttpGet]
+        public JsonResult PopulationChartQuarter()
+        {
+            var populationList = GetCityPopulationListQuarterAsync().Result;
+            return Json(populationList);
+        }
+        [HttpGet]
+        public JsonResult PopulationChartMonth()
+        {
+            var populationList = GetCityPopulationListMonthAsync().Result;
             return Json(populationList);
         }
     }
---
+++ Pharmacy/Pharmacy/Controllers/DrugAndQuantitiesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.Data;
+using Pharmacy.Models.Entities;
+
+namespace Pharmacy.Controllers
+{
+    public class DrugAndQuantitiesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DrugAndQuantitiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DrugAndQuantities
+        public async Task<IActionResult> Index()
+        {
+            ViewData["PharmacyList"] = await _context.tbPharmacys.ToListAsync();
+            return View(await _context.DrugAndQuantity.Include(x => x.Drug).ToListAsync());
+        }
+
+        // GET: DrugAndQuantities/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var drugAndQuantities = await _context.DrugAndQuantity
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (drugAndQuantities == null)
+            {
+                return NotFound();
+            }
+
+            return View(drugAndQuantities);
+        }
+
+        // GET: DrugAndQuantities/Create
+        public async Task<IActionResult> Create()
+        {
+            ViewData["DrugList"] = await _context.tbDrugs.ToListAsync();
+            ViewData["PharmacyList"] = await  _context.tbPharmacys.ToListAsync();
+            return View();
+        }
+
+        // POST: DrugAndQuantities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(long? drugId, long? quantity, long? pharmacy)
+        {
+            var drugAndQuantities = new DrugAndQuantities();
+            drugAndQuantities.Drug = _context.tbDrugs.Find(drugId);
+            drugAndQuantities.PharmacyId = (long) pharmacy;
+            drugAndQuantities.Quantity = (uint) quantity;
+            
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(drugAndQuantities);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(drugAndQuantities);
+        }
+
+        // GET: DrugAndQuantities/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var drugAndQuantities = await _context.DrugAndQuantity.FindAsync(id);
+            if (drugAndQuantities == null)
+            {
+                return NotFound();
+            }
+            return View(drugAndQuantities);
+        }
+
+        // POST: DrugAndQuantities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,PharmacyId,Quantity")] DrugAndQuantities drugAndQuantities)
+        {
+            if (id != drugAndQuantities.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(drugAndQuantities);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DrugAndQuantitiesExists(drugAndQuantities.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(drugAndQuantities);
+        }
+
+        // GET: DrugAndQuantities/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var drugAndQuantities = await _context.DrugAndQuantity
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (drugAndQuantities == null)
+            {
+                return NotFound();
+            }
+
+            return View(drugAndQuantities);
+        }
+
+        // POST: DrugAndQuantities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var drugAndQuantities = await _context.DrugAndQuantity.FindAsync(id);
+            _context.DrugAndQuantity.Remove(drugAndQuantities);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DrugAndQuantitiesExists(long id)
+        {
+            return _context.DrugAndQuantity.Any(e => e.Id == id);
+        }
+    }
+}
