16e67c855c9bcd290198b8c7885376fce0b71724
Uros Milovanovic <urkem98@gmail.com>
2021-02-07 20:44:19 +0100

    Rating

--- Pharmacy/Pharmacy/Controllers/RatingsController.cs
+++ Pharmacy/Pharmacy/Controllers/RatingsController.cs
@@ -2,27 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Pharmacy.Data;
 using Pharmacy.Models.Entities;
+using Pharmacy.Models.Entities.Users;
 
 namespace Pharmacy.Controllers
 {
     public class RatingsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
 
-        public RatingsController(ApplicationDbContext context)
+        public RatingsController(UserManager<AppUser> userManager, ApplicationDbContext context)
         {
+            _userManager = userManager;
             _context = context;
         }
-
+        [Authorize]
         // GET: Ratings
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Rating.ToListAsync());
+            var loggedInUser = await _userManager.GetUserAsync(User);
+
+            return View(await _context.Rating
+                .Include(x => x.Drug)
+                .Include(x => x.Employee)
+                .Include(x => x.Pharmacy)
+                .Include(x => x.User)
+                .Where(x => x.User.Id == loggedInUser.Id)
+                .ToListAsync());
         }
 
         // GET: Ratings/Details/5
@@ -42,10 +55,16 @@ namespace Pharmacy.Controllers
 
             return View(rating);
         }
-
-        // GET: Ratings/Create
-        public IActionResult Create()
+        [Authorize]
+        // GET: Ratings/CreateEmployee
+        public async Task<IActionResult> CreateEmployee()
         {
+            var loggedInUser = await _userManager.GetUserAsync(User);
+
+            var employee = await _context.tbAppointments.Where(x => x.PatientID == loggedInUser.Id).Select(x => x.MedicalExpertID).ToListAsync();
+
+            ViewData["EmployeeList"] = await _context.AppUsers.Where(x => employee.Contains(x.Id)).ToListAsync();
+
             return View();
         }
 
@@ -54,16 +73,86 @@ namespace Pharmacy.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Score")] Rating rating)
+        public async Task<IActionResult> CreateEmployee(string? employeeId, int? score)
         {
-            if (ModelState.IsValid)
+            if(employeeId != null && score != null)
             {
+                //new rating
+                Rating rating = new Rating();
+                rating.Employee = await _context.AppUsers.FindAsync(employeeId);
+                rating.Score = (int)score;
+                rating.User = await _userManager.GetUserAsync(User);
                 _context.Add(rating);
                 await _context.SaveChangesAsync();
+
+                //modify employee
+                rating.Employee.AverageScore = _context.Rating.Where(m => m.Employee.Id == rating.Employee.Id).Select(m => (float)m.Score).Average();
+                _context.Update(rating.Employee);
+
                 return RedirectToAction(nameof(Index));
             }
-            return View(rating);
+            else
+            {
+                return RedirectToAction(nameof(CreateEmployee));
+            }
         }
+
+
+        [Authorize]
+        // GET: Ratings/CreateEmployee
+        public async Task<IActionResult> CreatePharmacy()
+        {
+            var loggedInUser = await _userManager.GetUserAsync(User);
+
+            List<long> pharmacyOrders = await _context.tbOrders
+                .Include(x => x.User)
+                .Include(x => x.DrugAndQuantities)
+                .Where(x => x.TransactionComplete && x.User.Id == loggedInUser.Id)
+                .Select(x => x.DrugAndQuantities.PharmacyId)
+                .ToListAsync();
+
+            List<long> pharmacyAppoitments = await _context.tbAppointments
+                .Where(x => x.PatientID == loggedInUser.Id)
+                .Select(x => x.PhrmacyId)
+                .ToListAsync();
+
+            pharmacyOrders.AddRange(pharmacyAppoitments);
+            List<long> allPharmacy = (List<long>) pharmacyOrders;
+
+            ViewData["PharmacyList"] = await _context.tbPharmacys.Where(x => allPharmacy.Contains(x.Id)).ToListAsync();
+
+            return View();
+        }
+
+        // POST: Ratings/CreatePharmacy
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePharmacy(long? pharmacyId, int? score)
+        {
+            if (pharmacyId != null && score != null)
+            {
+                //new rating
+                Rating rating = new Rating();
+                rating.Pharmacy = await _context.tbPharmacys.FindAsync(pharmacyId);
+                rating.Score = (int)score;
+                rating.User = await _userManager.GetUserAsync(User);
+                _context.Add(rating);
+                await _context.SaveChangesAsync();
+
+                //modify employee
+                rating.Pharmacy.AverageScore = _context.Rating.Where(m => m.Pharmacy.Id == rating.Pharmacy.Id).Select(m => (float)m.Score).Average();
+                _context.Update(rating.Pharmacy);
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return RedirectToAction(nameof(CreatePharmacy));
+            }
+        }
+
 
         // GET: Ratings/Edit/5
         public async Task<IActionResult> Edit(long? id)
