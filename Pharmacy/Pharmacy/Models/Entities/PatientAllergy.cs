402b7c60e0dd55986f7aefdf532ac1f6f03e11a2
Filip <filipzdelar@gmail.com>
2021-02-07 22:55:29 +0100

    Patient allragy model

---
+++ Pharmacy/Pharmacy/Models/Entities/PatientAllergy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Models.Entities
+{
+    [Table("PatientAllergies")]
+    public class PatientAllergy
+    {
+        public long Id { get; set; }
+
+        [ForeignKey("AppUser")]
+        public string PatientID { get; set; }
+
+        [ForeignKey("Drug")]
+        public long DrugID { get; set; }
+    }
+}
