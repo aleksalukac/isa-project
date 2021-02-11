b12f5a0ccabcad46c134d5e11aaaca6fdce8b20c
Filip <filipzdelar@gmail.com>
2021-02-11 14:00:59 +0100

    integration test

--- Pharmacy/NUnitTestPharmacy/UnitTest1.cs
+++ Pharmacy/NUnitTestPharmacy/UnitTest1.cs
@@ -12,6 +12,13 @@ using Pharmacy.Models.Entities.Users;
 using System.Web.Http.Results;
 using Pharmacy.Data;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
 
 namespace NUnitTestPharmacy
 {
@@ -24,7 +31,19 @@ namespace NUnitTestPharmacy
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
         private readonly ApplicationDbContext _context;
+        private IWebDriver _webDriver;
+        private WebDriverWait _wait;
+        private int _timeoutInSeconds = 30;
+        private static string _driverPath = "C:\\Users\\Panonit\\Desktop\\Isa\\isa-projeka_LastVersion\\isa-project\\Pharmacy\\NUnitTestPharmacy\\WebDriverGoogleChome\\";
 
+        [SetUp]
+        public void SetUp()
+        {
+            _webDriver = new ChromeDriver(_driverPath);
+            _wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(_timeoutInSeconds));
+        }
+
+        #region Unit Test
         [Test]
         public async Task InvalideDrugFormatValide()
         {
@@ -33,5 +52,64 @@ namespace NUnitTestPharmacy
             var actionResult = await drugsController.Create(drug);
             Assert.IsInstanceOf<BadRequestObjectResult>(actionResult, typeof(BadRequestObjectResult).ToString());
         }
+        #endregion
+
+        #region Integration  Test
+
+        public bool LogInEmployee(IWebDriver webDriver, WebDriverWait wait, LoginDTO loginDTO)
+        {
+            bool result = false;
+            try
+            {
+                webDriver.Navigate().GoToUrl("https://localhost:44396/Identity/Account/Login");
+                webDriver.Manage().Window.Maximize();
+                IWebElement webElement;
+                Thread.Sleep(1000);
+
+                // Username
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("username_input")));
+                webElement = webDriver.FindElement(By.Id("username_input"));
+                webElement.Clear();
+                webDriver.FindElement(By.Id("username_input")).SendKeys(loginDTO.username);
+
+                // password
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("password_input")));
+                webElement = webDriver.FindElement(By.Id("password_input"));
+                webElement.Clear();
+                webDriver.FindElement(By.Id("password_input")).SendKeys(loginDTO.password);
+
+                Thread.Sleep(1000);
+                webDriver.FindElement(By.Id("submit_login")).Click();
+                Thread.Sleep(5000);
+
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("discort_element")));
+                var element = webDriver.FindElement(By.Id("discort_element")).Displayed;
+                if (element)
+                {
+                    result = true;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Source + " - " + ex.Message + " - " + ex.StackTrace);
+                return result;
+            }
+            finally
+            {
+                
+            }
+            return result;
+
+        }
+
+        [Test]
+        public async Task LoginTest()
+        {
+            LoginDTO loginDTO = new LoginDTO("hafik45066@hrandod.com", "Admin.123");
+            var results = LogInEmployee(_webDriver, _wait, loginDTO);
+            Assert.IsTrue(results);
+        }
+        #endregion
     }
 }
